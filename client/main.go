46b148e310ac782aabc59ce7ea8922c3e440b74f
Sona <sonasivasundari@gmail.com>
2023-09-06 15:26:10 +0530

    commitd

--- client/main.go
+++ client/main.go
@@ -4,64 +4,76 @@ import (
 	"context"
 	"fmt"
 	h "inventory/proto"
-	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
 )
 
 func main() {
+	r := gin.Default()
 	conn, err := grpc.Dial("localhost:2023", grpc.WithInsecure())
 	if err != nil {
 		fmt.Println("Failed to connect: ", err)
 	}
 	defer conn.Close()
 	client := h.NewInventoryServiceClient(conn)
-	// req := &h.AllInventoryItems{
-	// 	Items: []*h.InventoryItem{
-	// 		{
-	// 			Id:   1,
-	// 			Item: "shirt",
-	// 			Features: []string{
-	// 				"slim fit",
-	// 				"full sleeves",
-	// 			},
-	// 			Categories: []string{
-	// 				"tshirt",
-	// 				"formal",
-	// 			},
-	// 		},
-	// 		{Id: 2,
-	// 			Item: "pants",
-	// 			Features: []string{
-	// 				"full length",
-	// 				"slim fit",
-	// 			},
-	// 			Categories: []string{
-	// 				"jeans",
-	// 				"formal",
-	// 			},
-	// 		},
-	// 		{Id: 3,
-	// 			Item: "headphones",
-	// 			Features: []string{
-	// 				"wireless",
-	// 				"noise cancellation",
-	// 			},
-	// 			Categories: []string{
-	// 				"headphones",
-	// 				"bluetooth",
-	// 			},
-	// 		},
-	// 	},
-	// }
-	// response, err := client.CreateInventory(context.Background(), req)
-	response, err := client.UpdateInventory(context.Background(), &h.ItemToDelete{
-		Item:     "shirt",
-		Sku:      "SKU002",
-		Quantity: 10,
+	r.GET("/getitems", func(c *gin.Context) {
+		response, err := client.GetAllItems(context.Background(), &h.Empty{})
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+		fmt.Println("Response: ", response)
+		c.JSON(http.StatusOK, gin.H{"value": response})
 	})
-	if err != nil {
-		log.Fatal("Failed to call: ", err)
-	}
-	fmt.Println("Response: ", response)
+
+	r.POST("/updateitems", func(c *gin.Context) {
+		var request h.ItemToDelete
+		if err := c.ShouldBindJSON(&request); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		response, err := client.UpdateInventory(context.Background(), &h.ItemToDelete{
+			Item:     request.Item,
+			Sku:      request.Sku,
+			Quantity: request.Quantity,
+		})
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+		fmt.Println("Response: ", response)
+		c.JSON(http.StatusOK, gin.H{"value": response})
+	})
+
+	r.POST("/getitem", func(c *gin.Context) {
+		var request h.ItemName
+		if err := c.ShouldBindJSON(&request); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		response, err := client.GetInventoryItemByItemName(context.Background(), &h.ItemName{
+			ItemName: request.ItemName,
+		})
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+		fmt.Println("Response: ", response)
+		c.JSON(http.StatusOK, gin.H{"value": response})
+	})
+	r.POST("/create", func(c *gin.Context) {
+		var request []*h.InventoryItem
+		if err := c.ShouldBindJSON(&request); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		response, err := client.CreateInventory(context.Background(), &h.AllInventoryItems{
+			Items: request,
+		})
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+		fmt.Println("Response: ", response)
+		c.JSON(http.StatusOK, gin.H{"value": response})
+	})
+	r.Run(":5000")
 }
